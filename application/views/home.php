f7fca4ed520e323c793f2a40cdfd54ab76595c01
João Victor da Silva <jv782063@gmail.com>
2021-08-18 14:50:54 -0300

    initial commit

---
+++ application/views/home.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+  </head>
+  <body>
+    <div>
+      <h1>Hello World!</h1>
+  </body>
+</html>
