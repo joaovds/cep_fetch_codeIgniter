7af93d8195e2c0b56f661c74cede8a55a9d90435
João Victor da Silva <jv782063@gmail.com>
2021-08-19 15:10:51 -0300

    create header

--- application/views/home.php
+++ application/views/home.php
@@ -6,10 +6,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('/assets/css/global.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('/assets/css/home.css') ?>">
     <title>Home</title>
   </head>
   <body>
-    <div>
-      <h1>Hello World!</h1>
+    <header>
+      <strong>
+        Seja bem-vindo(a), João!
+      </strong>
+
+      <div>
+        <span>Qui, 19 Agosto</span>
+        <a href="#">
+          Sair
+        </a>
+      </div>
+    </header>
+
+    <div class="container">
+    </div>
   </body>
 </html>
