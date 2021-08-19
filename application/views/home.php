ba89ea0970992b1d60f8d74de8fca2a19d1e534f
João Victor da Silva <jv782063@gmail.com>
2021-08-19 18:00:54 -0300

    makes the body of the page, showing user information

--- application/views/home.php
+++ application/views/home.php
@@ -4,27 +4,58 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= base_url('/assets/css/global.css') ?>">
-    <link rel="stylesheet" href="<?= base_url('/assets/css/home.css') ?>">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="<?= base_url('/assets/css/global.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url('/assets/css/home.css') ?>" />
     <title>Home</title>
   </head>
   <body>
     <header>
-      <strong>
-        Seja bem-vindo(a), João!
-      </strong>
+      <span>Qui, 19 Agosto</span>
 
-      <div>
-        <span>Qui, 19 Agosto</span>
-        <a href="#">
-          Sair
-        </a>
-      </div>
+      <strong> Seja bem-vindo(a), João! </strong>
+
+      <a href="#"> Sair </a>
     </header>
 
-    <div class="container">
-    </div>
+    <section class="container">
+      <header>
+        <h1>Suas informações:</h1>
+        <a href="#">Editar</a>
+      </header>
+
+      <main>
+        <div>
+          <div class="input-group">
+            <span>Estado</span>
+            <input type="text" readonly>
+          </div>
+
+          <div class="input-group">
+            <span>Bairro</span>
+            <input type="text" readonly>
+          </div>
+        </div>
+
+        <div>
+          <div class="input-group">
+            <span>Cidade</span>
+            <input type="text" readonly>
+          </div>
+
+          <div class="input-group-2">
+            <div class="input-group">
+              <span>Rua</span>
+              <input type="text" readonly>
+            </div>
+            <div class="input-group">
+              <span>Número</span>
+              <input type="number" readonly>
+            </div>
+          </div>
+        </div>
+      </main>
+    </section>
   </body>
 </html>
