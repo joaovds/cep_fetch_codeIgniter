41a5571d53e7d71f3ee1bd474da5e49714e10bca
João Victor da Silva <jv782063@gmail.com>
2021-08-23 20:15:49 -0300

    create login screen

---
+++ application/views/login.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="<?= base_url('/assets/css/global.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url('/assets/css/login.css') ?>">
+    <title>Login</title>
+  </head>
+  <body>
+    <div class="container">
+      <form>
+        <h1>Login</h1>
+
+        <div class="input-group">
+          <span>Email</span>
+          <input type="email">
+        </div>
+        <div class="input-group">
+          <span>Senha</span>
+          <input type="password">
+        </div>
+
+        <button type="submit">
+          Entrar
+        </button>
+
+        <span>
+          Ainda não tem conta? <a href="#">Crie uma agora</a>
+        </span>
+      </form>
+    </div>
+  </body>
+</html>
